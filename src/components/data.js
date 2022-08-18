a2f81c8dd3c681b1dc7006541586d9f27dffb00f
SaishJ <saishjagtap0003@gmail.com>
2022-08-18 16:29:22 +0530

    :building_construction: Changing Image url
    Changing Image url

--- src/components/data.js
+++ src/components/data.js
@@ -2,42 +2,48 @@ export const projects = [
   {
     id: 1,
     title: "Cordova Calculator",
-    image: "https://www.linkpicture.com/q/calculator_3.jpg",
+    image:
+      "https://user-images.githubusercontent.com/59008917/185378396-01630ee9-b524-43dd-9d43-bf9b30641721.jpg",
     code: "https://github.com/SaishJ/All-In-One-Calculator-Cordova-",
     live: "https://github.com/SaishJ/All-In-One-Calculator-Cordova-",
   },
   {
     id: 2,
     title: "Rupesh Lights",
-    image: "https://www.linkpicture.com/q/rupesh-lights.png",
+    image:
+      "https://user-images.githubusercontent.com/59008917/185378466-eafe76d8-8e5d-45c9-9e34-04aa00c45e41.png",
     code: "https://github.com/SaishJ/RupeshLights",
     live: "https://rupeshlights.netlify.app/",
   },
   {
     id: 3,
     title: "Screenshot Downloader",
-    image: "https://www.linkpicture.com/q/screenshot-generator.png",
+    image:
+      "https://user-images.githubusercontent.com/59008917/185378488-0b56d011-f274-4f15-bec3-4c0119844bd4.png",
     code: "https://github.com/SaishJ/Screenshot-Downloader-App",
     live: "https://saishj.github.io/Screenshot-Downloader-App/",
   },
   {
     id: 4,
     title: "React Native Chat Application",
-    image: "https://www.linkpicture.com/q/chat-app.png",
+    image:
+      "https://user-images.githubusercontent.com/59008917/185378411-e7f78b46-1979-442e-b455-1fe5c3970a12.png",
     code: "https://github.com/SaishJ/ReactNative-ChatApp",
     live: "https://github.com/SaishJ/ReactNative-ChatApp/blob/master/android/app/build/outputs/apk/release/app-release.apk",
   },
   {
     id: 5,
     title: "Currency Converter Cordova",
-    image: "https://www.linkpicture.com/q/curr-con.png",
+    image:
+      "https://user-images.githubusercontent.com/59008917/185378445-d5e0b671-7ff1-42c4-8e06-fcc2800529ea.png",
     code: "https://github.com/SaishJ/Currency-Converter-Cordova-",
     live: "https://github.com/SaishJ/Currency-Converter-Cordova-",
   },
   {
     id: 6,
     title: "To Do List",
-    image: "https://www.linkpicture.com/q/LinkedIn-cover-1-1.png",
+    image:
+      "https://user-images.githubusercontent.com/59008917/185378497-83f9d025-1793-4e4a-9357-2f03df3ccb97.png",
     code: "https://github.com/SaishJ/ToDo-List",
     live: "https://github.com/SaishJ/ToDo-List",
   },
